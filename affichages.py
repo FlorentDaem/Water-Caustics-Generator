03794e8b24bab5f4893dcb3b6af7cd2739399ec1
Florent <florentdaem.fd@gmail.com>
2023-01-08 19:23:44 +0100

    documentation

--- affichages.py
+++ affichages.py
@@ -8,17 +8,17 @@ from constantes import *
 
 def indices_du_point(P):
     """
-    Renvoie les indices du pixel qui correspond au point P
+    Renvoie les indices du pixel qui correspond au point P.
 
     Parameters
     ----------
-    P : Array numpy
-        Coordonnées du point P
+    P : array
+        Coordonnées du point P.
 
     Returns
     -------
     (int, int)
-        Indices i et j tels que (i*dx, j*dy) = P
+        Indices i et j tels que (i*dx, j*dy) = P.
     """
     i = int(np.dot(P, np.array([1, 0, 0]))/dx)
     j = int(np.dot(P, np.array([0, 1, 0]))/dy)
@@ -26,22 +26,33 @@ def indices_du_point(P):
 
 
 def affiche_rayons(rayons, surface, save=False):
-    '''Dessine les rayons et l'eau dans le plan y=0.'''
+    """
+    Dessine les rayons et l'eau dans le plan y=0.
+
+    Parameters
+    ----------
+    rayons : list
+        Liste d'objets rayon.
+    surface : array
+        Surface d'eau.
+    save : bool, optional
+        Enregistre la figure si save==True, by default False.
+    """
     for i in range(Nx-1):
         for j in range(Ny-1):
             rayon = rayons[i][j]
             point_source = rayon.point_source
             point_interface = rayon.point_interface
             point_sol = rayon.point_sol
-            iL, jL = indices_du_point(rayon.point_source)
-            # iS, jS = indices_du_point(point_sol)
+            i, j = indices_du_point(rayon.point_source)
 
-            if jL == 0:
+            # Plot les rayons du plan y=0
+            if j == 0:
                 plt.plot([point_source[0], point_interface[0], point_sol[0]], [point_source[2], point_interface[2], point_sol[2]], color='red')
 
     plt.plot(vals_x, surface[:, 0])
 
-    plt.xlim(-Lx/2*0, Lx/2*2)
+    plt.xlim(0, Lx)
     plt.ylim(0, Lz)
     if save:
         nom_dossier = "images"
@@ -49,38 +60,70 @@ def affiche_rayons(rayons, surface, save=False):
         plt.savefig(os.path.join(nom_dossier, nom_fichier))
 
 
-def plot_surface(surface, fact=1):
+def plot_surface(surface):
+    """
+    Plot la surface d'eau en 3D.
+
+    Parameters
+    ----------
+    surface : array
+        Surface d'eau.
+    """
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim(-Lx/2*0, Lx/2*2)
-    ax.set_ylim(-Ly/2*0, Ly/2*2)
+    ax.set_xlim(0, Lx)
+    ax.set_ylim(0, Ly)
     ax.set_zlim(0, Lz)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
-    ax.plot_surface(grille_X, grille_Y, (surface-H)*fact + H, cmap="Blues",
+    ax.plot_surface(grille_X, grille_Y, surface, cmap="Blues",
                     linewidth=0, antialiased=False, alpha=0.9)
     plt.close(fig)
 
 
-def save_frame_surface(surface, n, nom_dossier, fact=1):
+def save_frame_surface(surface, n, nom_dossier):
+    """
+    Enregistre une frame du plot de la surface d'eau en 3D.
+
+    Parameters
+    ----------
+    surface : array
+        Surface d'eau.
+    n : int
+        Numéro de la frame.
+    nom_dossier : str
+        Nom du dossier d'enregistrement.
+    """
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim(-Lx/2*0, Lx/2*2)
-    ax.set_ylim(-Ly/2*0, Ly/2*2)
+    ax = fig.add_subplot(111, projection="3d")
+    ax.set_xlim(0, Lx)
+    ax.set_ylim(0, Ly)
     ax.set_zlim(0, Lz)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
-    ax.plot_surface(grille_X, grille_Y, (surface-H)*fact + H, cmap="Blues",
+    ax.plot_surface(grille_X, grille_Y, surface, cmap="Blues",
                     linewidth=0, antialiased=False, alpha=0.9)
-    # nom_dossier = "frames"
     nom_fichier = f"frame{n}.png"
     fig.savefig(os.path.join(nom_dossier, nom_fichier))
     plt.close(fig)
 
 
 def calcul_motifs(rayons):
+    """
+    Détermine la luminosité du motif.
+
+    Parameters
+    ----------
+    rayons : list
+        Liste de rayons.
+
+    Returns
+    -------
+    array
+        Image du motif.
+    """
     motif = np.zeros((Nx, Ny))
 
     for i in range(Nx-1):
@@ -89,11 +132,12 @@ def calcul_motifs(rayons):
             point_sol = rayon.point_sol
             i_S, j_S = indices_du_point(point_sol)
 
+            # Ajoute de la luminosité à chaque pixel dans l'intervalle considéré
             if (0 <= i_S and i_S < Nx-1) and (0 <= j_S and j_S < Ny-1):
                 motif[i_S, j_S] += rayon.lum_sol
 
     max_I = motif.max()
-    # règle l'intensité de la lumière en fonction du nombre d'impacts de rayons
+    # Règle l'intensité de la lumière en fonction du nombre d'impacts de rayons
     motif[:, :] = motif[:, :]/max_I
     return motif
 
@@ -122,10 +166,23 @@ def motif_to_alpha(motif):
 
 
 def save_image(surface, rayons, n, nom_dossier):
+    """
+    Sauvegarde une frame du motif.
+
+    Parameters
+    ----------
+    surface : array
+        Surface d'eau.
+    rayons : list
+        Liste d'objets rayons.
+    n : int
+        Numéro de la frame.
+    nom_dossier : str
+        Nom du dossier d'enregistrement.
+    """
     motif = calcul_motifs(rayons)
 
     image = motif_to_alpha(motif)
-    # nom_dossier = "frames"
     nom_fichier = f"frame{n} image.png"
     plt.imsave(os.path.join(nom_dossier, nom_fichier), image)
     plt.close()
